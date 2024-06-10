b27b84213f0493172a52f74ebddc46d915535530
UgurONAL <ugur.96@outlook.com>
2024-06-10 03:01:59 +0300

    Sokoban Solver is finished but it is so slow.

--- src/sokoban_solver.py
+++ src/sokoban_solver.py
@@ -11,8 +11,9 @@ class SokobanSolver(Problem):
         """The constructor specifies tdhe initial state, and possibly a goal
         state, if there is a unique goal. Your subclass's constructor can add
         other arguments."""
-        super().__init__(initial, self.player_position(initial))
-        self.m, self.n = len(initial), len(initial[0])
+        super().__init__((self.convert_state_to_search_state(initial),
+                          self.player_position(initial)))
+        self.m, self.n = len(initial), max([len(row) for row in initial])
         self.directions = [(0, 1), (1, 0), (0, -1), (-1, 0)]
         self.solution_node = None
 
@@ -20,8 +21,29 @@ class SokobanSolver(Problem):
     def player_position(state):
         for i, row in enumerate(state):
             for j, cell in enumerate(row):
-                if cell == '@':
+                if '@' in cell:
                     return i, j
+
+    @staticmethod
+    def convert_state_to_search_state(state):
+        search_state = []
+        for row in state:
+            search_row = []
+            for cell in row:
+                search_cell = [cell]
+                if cell in ["@", "$"]:
+                    search_cell.append(" ")
+                search_row.append(tuple(search_cell))
+            search_state.append(tuple(search_row))
+        return tuple(search_state)
+
+    @staticmethod
+    def state_to_tuple(state):
+        return tuple([tuple([tuple(cell) for cell in row]) for row in state])
+
+    @staticmethod
+    def state_to_list(state):
+        return [[list(cell) for cell in row] for row in state]
 
     def actions(self, state):
         """Return the actions that can be executed in the given
@@ -32,8 +54,14 @@ class SokobanSolver(Problem):
         possible_actions = []
         for di, dj in self.directions:
             new_i, new_j = player_i + di, player_j + dj
-            if 0 <= new_i < self.m and 0 <= new_j < self.n and state[0][new_i][new_j] != "#":
-                if state[0][new_i][new_j] == "." and state[0][new_i + di][new_j + dj] not in [".", "#"]:
+            if 0 <= new_i < self.m and 0 <= new_j < len(state[0][new_i]) and state[0][new_i][new_j] != "#":
+                if len(state[0][new_i][new_j]) == 1 and (" " in state[0][new_i][new_j] or
+                                                         "." in state[0][new_i][new_j]):
+                    possible_actions.append((di, dj))
+                if ("$" in state[0][new_i][new_j] and
+                        0 <= new_i + di < self.m and 0 <= new_j + dj < len(state[0][new_i]) and
+                        "$" not in state[0][new_i + di][new_j + dj] and
+                        "#" not in state[0][new_i + di][new_j + dj]):
                     possible_actions.append((di, dj))
 
         return possible_actions
@@ -42,21 +70,67 @@ class SokobanSolver(Problem):
         """Return the state that results from executing the given
         action in the given state. The action must be one of
         self.actions(state)."""
-        raise NotImplementedError
+        player_i, player_j = state[1]
+        di, dj = action
+        new_i, new_j = player_i + di, player_j + dj
+
+        new_state = self.state_to_list(state[0])
+
+        new_state[player_i][player_j].remove("@")
+        if "$" in new_state[new_i][new_j]:
+            new_state[new_i][new_j].remove("$")
+            new_state[new_i + di][new_j + dj].append("$")
+        new_state[new_i][new_j].append("@")
+
+        return self.state_to_tuple(new_state), (new_i, new_j)
 
     def goal_test(self, state):
         """Return True if the state is a goal. The default method compares the
         state to self.goal or checks for state in self.goal if it is a
         list, as specified in the constructor. Override this method if
         checking against a single self.goal is not enough."""
-        raise NotImplementedError
+        return len(SokobanSolver.get_unplaced_crate_locations(state)) == 0
 
-    def h(self, node):
+    @staticmethod
+    def get_unplaced_crate_locations(state):
+        unplaced_crates = []
+        for i, row in enumerate(state[0]):
+            for j, cell in enumerate(row):
+                if "." in cell and "$" not in cell:
+                    unplaced_crates.append((i, j))
+        return unplaced_crates
+
+    @staticmethod
+    def get_empty_storage_place_locations(state):
+        empty_storage_places = []
+        for i, row in enumerate(state[0]):
+            for j, cell in enumerate(row):
+                if "$" in cell and "." not in cell:
+                    empty_storage_places.append((i, j))
+        return empty_storage_places
+
+    @staticmethod
+    def manhattan_distance(a, b):
+        return abs(a[0] - b[0]) + abs(a[1] - b[1])
+
+    @staticmethod
+    def h(node):
         """Return the heuristic value for a given state. Default is 0."""
-        return 0
+        unplaced_crate_locations = SokobanSolver.get_unplaced_crate_locations(node.state)
+        empty_storage_place_locations = SokobanSolver.get_empty_storage_place_locations(node.state)
+        value = 0
+
+        for crate_location in unplaced_crate_locations:
+            for storage_place_location in empty_storage_place_locations:
+                value += SokobanSolver.manhattan_distance(crate_location, storage_place_location)
+        value /= len(unplaced_crate_locations) if len(unplaced_crate_locations) > 0 else 1
+        value += min([SokobanSolver.manhattan_distance(node.state[1], unplaced_crate_location)
+                     for unplaced_crate_location in unplaced_crate_locations]) - 1 if len(unplaced_crate_locations) > 0 else 1
+
+        return value
 
     def solve(self):
-        self.solution_node = astar_search(self)
+        self.solution_node = astar_search(self, h=self.h)
         return self.solution_node is not None
 
     def solution(self):
--- src/utilities.py
+++ src/utilities.py
@@ -44,7 +44,7 @@ def get_system_info():
             "content": "You are a Sokoban level generator. "
                        "The level is represented as a grid of characters, with specific symbols representing different elements of the game. "
                        "Here are the symbols used: - `#` represents a wall. - `@` represents the player. "
-                       "- `$` represents a storage location. - `.` represents a crate. - ` ` (space) represents an empty space. "
+                       "- `.` represents a storage location. - `$` represents a crate. - ` ` (space) represents an empty space. "
                        "Each level should be a solvable puzzle where all crates can be moved to storage locations. "
                        "The player should start at a reasonable position within the level. "
                        "There should be only one player in a level. The number of crates and storage locations should be equal."}
@@ -53,8 +53,8 @@ def get_system_info():
 def get_system_info_text():
     return "{" + (f"\"role\": \"system\", \"content\": \"You are a Sokoban level generator. "
                   f"The level is represented as a grid of characters, with specific symbols representing different elements of the game. "
-                  f"Here are the symbols used: - `#` represents a wall. - `@` represents the player. - `$` represents a storage location. "
-                  f"- `.` represents a crate. - ` ` (space) represents an empty space. "
+                  f"Here are the symbols used: - `#` represents a wall. - `@` represents the player. - `.` represents a storage location. "
+                  f"- `$` represents a crate. - ` ` (space) represents an empty space. "
                   f"Each level should be a solvable puzzle where all crates can be moved to storage locations. "
                   f"The player should start at a reasonable position within the level. "
                   f"There should be only one player in a level. "
@@ -92,20 +92,31 @@ def create_jsonl_from_folder(folder_path, output_file):
         file.write("\n".join(data))
 
 
-def check_level_validity(level_matrix):
+def get_intended_dimensions_from_filename(filename):
+    return tuple(filename.split("_")[-1].split(".")[0].split("x"))
+
+
+def check_level_validity(level_matrix, intended_dimensions):
+    dimension_check = (len(level_matrix) == intended_dimensions[0] and
+                       max([len(row) for row in level_matrix]) == intended_dimensions[1])
+
     actor_counts = {"@": 0, "$": 0, ".": 0, " ": 0, "#": 0}
     for row in level_matrix:
         for cell in row:
             if cell in actor_counts:
                 actor_counts[cell] += 1
 
-    if actor_counts["@"] != 1:
-        return False, "The level should contain exactly one player."
-    if actor_counts["$"] != actor_counts["."]:
-        return False, "The number of crates and storage locations should be equal."
+    player_count_check = actor_counts["@"] == 1
+    crate_storage_location_count_check = actor_counts["$"] == actor_counts["."]
 
-    """sokoban_solver = SokobanSolver(level_matrix)
-    if not sokoban_solver.solve():
-        return False, "The level is unsolvable."
+    if player_count_check and crate_storage_location_count_check:
+        sokoban_solver = SokobanSolver(level_matrix)
+        solvable_check = sokoban_solver.solve()
+        solution = None
+        if solvable_check:
+            solution = sokoban_solver.solution()
+    else:
+        solvable_check = False
+        solution = None
 
-    return True, sokoban_solver.solution_node()"""
+    return dimension_check, player_count_check, crate_storage_location_count_check, solvable_check, solution
